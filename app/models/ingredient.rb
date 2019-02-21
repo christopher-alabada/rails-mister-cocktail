f65742ed1b9f579d28cb3e7a382b75c3110bfaa5
Christopher Alabada <christopher.alabada@gmail.com>
2019-02-21 17:31:42 +0900

    Continue...

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,6 @@ class Ingredient < ApplicationRecord
   has_many :doses
 
   validates :name, presence: true, uniqueness: true
+
+  NAMES = Ingredient.order(:name).all.map { |ingredient| [ingredient.name, ingredient.id] }
 end
